b68be3046fef71842a06fedf9c7cfa63bdcbbdee
JunMing27 <98100812+JunMing27@users.noreply.github.com>
2022-06-01 21:58:17 +0800

    Update iframecontestRequestOrg.php

--- iframecontestRequestOrg.php
+++ iframecontestRequestOrg.php
@@ -34,7 +34,7 @@ $sql_organiserQuery = mysqli_query($con,$sql_organiser);
 $organiserInfo = mysqli_fetch_array($sql_organiserQuery);
 
 // organiser information
-$organiserId = $organiserInfo['contestName'];
+$organiserId = $organiserInfo['organiserID'];
 
             require "conn.php";
             $sql_allContest = "SELECT contestId, contestName, approvalStatus, startDate FROM contest WHERE organiserID='$organiserId' AND approvalStatus = 'pending';";
